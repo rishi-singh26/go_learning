ebddda83eb9b31b9d78c1c5f2c9351849b511e7a
rishi-singh26 <rishisingh0831@gmail.com>
2024-06-16 11:13:44 +0530

    Formatting Strings - Basics

--- go_essentials/investment_calculator/investment_calculator.go
+++ go_essentials/investment_calculator/investment_calculator.go
@@ -19,6 +19,7 @@ func main() {
     futureValue := investmentAmount * math.Pow(1 + (expectedReturnRate / 100), years)
     futureRealValue := futureValue / math.Pow(1 + inflationRate / 100, years)
 
-    fmt.Println(futureValue)
-    fmt.Println(futureRealValue)
+    // fmt.Println("Future Value: ", futureValue)
+    fmt.Printf("Future Value: %v\nFuture Value: (Adjusted for inflation): %v", futureValue, futureRealValue)
+    // fmt.Println("Future Value: (Adjusted for inflation) ", futureRealValue)
 }
