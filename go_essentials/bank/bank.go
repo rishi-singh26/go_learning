526e2be7252abf3020bab3d44eb44eb7ac72430c
rishi-singh26 <rishisingh0831@gmail.com>
2024-07-12 07:54:45 +0530

    Moved code to seperate file in the same package

--- go_essentials/bank/bank.go
+++ go_essentials/bank/bank.go
@@ -72,13 +72,3 @@ func readFromFile() (float64, error) {
 	}
 	return balance, nil
 }
-
-func getUserSelection() (selection int) {
-	fmt.Println("What do you want to do?")
-	fmt.Println("1. Check banlance")
-	fmt.Println("2. Deposit money")
-	fmt.Println("3. Withdraw money")
-	fmt.Println("4. Exit")
-	fmt.Scan(&selection)
-	return
-}
